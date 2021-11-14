2c481416661b6b2855ee6ea9d1c0168ed6ef7d22
Mona Lisa <Santiago>
2021-11-14 11:57:45 -0500

    actualizacion texto pagina que es

--- src/pages/que_es.jsx
+++ src/pages/que_es.jsx
@@ -17,10 +17,10 @@ const Que_es_page = () => {
                 </div>           
                 <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4">
                     <p class="text-center ">
-                    <div className="font-bold text-xl mb-2">
+                    <div className="font-bold text-4xl mb-8 p-1">
                         <p>Que es ControlSoft.</p>
                     </div>
-                        <p className="text-gray-700 text-base">
+                        <p className="text-gray-700 text-base p-8">
                         El sistema automatizado de ventas es un lugar donde un cliente paga por un artículo o un servicio 
                             separado ofrecido por una compañía. Entonces, básicamente, es el sistema que permite a las partes 
                             proceder con la transacción entre un cliente y una compañía. Además, este término pertenece a una 
