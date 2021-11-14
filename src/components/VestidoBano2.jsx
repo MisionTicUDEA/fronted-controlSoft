3171429795b737930ebf85b7e4018c5ae1d46a47
Mona Lisa <Santiago>
2021-11-14 10:59:06 -0500

    actualizacion imagenes index privado

--- src/components/VestidoBano2.jsx
+++ src/components/VestidoBano2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from 'media/Vestido_bano4.jpg';
 
 const VestidoBano2 = () => {
-  return <img width="50%" height="70%" src={Logo} alt='Workflow' />;
+  return <img width="85%" height="105%" src={Logo} alt='Vestido de baño' />;
 };
 
 export default VestidoBano2;
--- src/pages/admin/Index.jsx
+++ src/pages/admin/Index.jsx
@@ -1,26 +1,37 @@
 import { useDarkMode } from 'context/darkMode';
 import React from 'react';
 import VestidoBano2  from 'components/VestidoBano2';
-import ImagenLogo from 'components/ImagenLogo';
+import VestidoBano3  from 'components/VestidoBano3';
+import VestidoBano4 from 'components/VestidoBano4';
 
 
 const Admin = ({ children }) => {
   const { darkMode } = useDarkMode();
   return <div>
   <div className={`flex h-full bg-gray-${darkMode ? '900' : '50'}`}>   
-<main>
-
-    <div className="flex flex-wrap"> 
-        <VestidoBano2  />
-        <ImagenLogo  />
-
-        {children}
+    <main>
+    <div className='flex  flex-col  p-2'>
+        <div className="font-bold text-3xl mb-8">
+          ControlSoft
+        </div>
       </div>
-     
-    
-      
-</main>
-</div>
+      <div className='flex  flex-col  p-2'>
+      <div class="grid grid-cols-1 md:grid-cols-3">
+        <div>
+      <VestidoBano2  />
+      </div>
+      <div>
+      <VestidoBano3  />
+      </div>
+      <div>
+      <VestidoBano4  />
+      </div>
+      </div>
+      </div>
+
+        {children}      
+    </main>
+  </div>
 </div>   
 };
 
